600e647f374680de824a057a51254b2976478556
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2017-10-29 16:19:47 -0700

    put in a boots ico so the bootstrapper has the right icon

--- release.py
+++ release.py
@@ -1,4 +1,4 @@
-import os, sys, shutil, subprocess
+import os, sys, shutil, subprocess, time
 
 version = "0.1"
 installName = 'Hover Practice'
@@ -6,22 +6,25 @@ installName = 'Hover Practice'
 # target is where we assemble our final install.
 if os.path.isdir('target/'):
     shutil.rmtree('target/')
+time.sleep(0.3)    
 installDir = 'target/' + installName + '/'
 os.mkdir('target/')
 #os.mkdir(installDir)
 
 os.chdir("src_bootstrapper")
-subprocess.call("cxfreeze.py bootstrapper.py --base-name=Win32GUI --target-dir dist", shell=True, stdout=sys.stdout, stderr=sys.stderr)
+subprocess.call("cxfreeze.py bootstrapper.py --base-name=Win32GUI --target-dir dist --icon ../boots.ico", shell=True, stdout=sys.stdout, stderr=sys.stderr)
 os.chdir("..")
 
 shutil.move('src_bootstrapper/dist/bootstrapper.exe', 'src_bootstrapper/dist/Hover Practice.exe') # Move the dist files to our target directory
 shutil.move('src_bootstrapper/dist/', installDir)
 
 
+os.chdir("src")
+subprocess.call("cxfreeze.py hoverpractice.py --base-name=Win32GUI --target-dir dist --icon ../boots.ico", shell=True, stdout=sys.stdout, stderr=sys.stderr)
+os.chdir("..")
 
-subprocess.call("cxfreeze.py hoverpractice.py --base-name=Win32GUI --target-dir dist", shell=True, stdout=sys.stdout, stderr=sys.stderr)
-shutil.copy('boots.png', 'dist/boots.png')
-shutil.move('dist/', installDir + 'hover-lib/')
+shutil.copy('boots.png', 'src/dist/boots.png')
+shutil.move('src/dist/', installDir + 'hover-lib/')
 
 shutil.copy('LICENSE.txt', installDir)
 shutil.copy('README.md', installDir + 'README.txt')
