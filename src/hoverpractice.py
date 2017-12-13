538f6ef4953d71a22006013218947b9ce8ee12d3
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2017-12-13 14:21:17 -0800

    animate the bars increasing. also redo some logic flow

--- src/hoverpractice.py
+++ src/hoverpractice.py
@@ -29,11 +29,14 @@ pad_amount = (stripe_width - bar_width) / 2
 cell_height = 10
 midline_y = window_h / 2
 
+max_top_cells = 30
+max_bottom_cells = 10
+
 stripe_in_progress = pygame.Surface((stripe_width, stripe_height))
 
 # we keep a history of the last N button presses to display
 history = []
-max_history_len = window_w // stripe_width
+max_history_len = window_w // stripe_width - 1 # one extra for the stripe in progress
 
 min_streak_length = 4 #Number of greens to be considered a streak
 min_good_streak_length = 10 #Number of greens to be considered a 'good' streak
@@ -65,7 +68,6 @@ def getDashButton():
                     return event.joy,event.button
         clock.tick(60)
                 
-
 joyid, buttonid = getDashButton()
 
 joy = pygame.joystick.Joystick(joyid)
@@ -80,57 +82,59 @@ while True:
                 
     color = red
     button_pressed = joy.get_button(buttonid)
-    if button_pressed != last_button_pressed:
-        last_button_pressed = button_pressed
-        if button_pressed:
-            # they've just pressed a button. now we draw a bar onto the stripe showing how long the button had been released.
-            # then we put the stripe into the history, which will get it drawn onscreen.
-            bar_height = button_frame_counter * cell_height
-            broken_streak = False
-            if button_frame_counter <= 1:
-                color = green
-                if hold_duration_check_passed:
-                    current_streak += 1
-                    if current_streak >= min_good_streak_length:
-                        last_good_streak = current_streak
-                    best_streak = max(current_streak, best_streak)
-                else:
-                    broken_streak = True
-            else:
-                broken_streak = True
-
-            if broken_streak:
-                if current_streak >= min_streak_length:
-                    last_streaks.append(current_streak)
-                if len(last_streaks) > rolling_average_size:
-                    last_streaks.pop(0)
-                current_streak = 0
-
-            pygame.draw.rect(stripe_in_progress, color, pygame.Rect(pad_amount, midline_y, bar_width, bar_height))
+    broken_streak = True
+    if button_pressed:
+        if not last_button_pressed:
+            button_frame_counter = 1
             history.append(stripe_in_progress)
             # when a stripe scrolls off the left side of the screen, get rid of it
             if len(history) > max_history_len:
-                history.pop(0)
-            button_frame_counter = 0               
-        else: 
-            # they just released a button. now we're going to make a new stripe and draw a bar onto it showing how long it had been pressed.
-            bar_height = button_frame_counter * cell_height
-            hold_duration_check_passed = False
-            if button_frame_counter <= 30:
-                color = green
-                hold_duration_check_passed = True
+                history.pop(0)                
             stripe_in_progress = pygame.Surface((stripe_width, stripe_height))
-            pygame.draw.rect(stripe_in_progress, color, pygame.Rect(pad_amount, midline_y - bar_height, bar_width, bar_height))
-            button_frame_counter = 0
+            
+        # button is currently held down, so draw the top bar
+        bar_height = min(button_frame_counter, max_top_cells) * cell_height
+        if button_frame_counter <= max_top_cells:
+            color = green
+            broken_streak = False
+                
+        pygame.draw.rect(stripe_in_progress, color, pygame.Rect(pad_amount, midline_y - bar_height, bar_width, bar_height))    
+    else: # button currently not pressed        
+        if last_button_pressed:
+            button_frame_counter = 1
+            
+        # draw the bottom bar            
+        bar_height = min(button_frame_counter, max_bottom_cells) * cell_height
+        if button_frame_counter <= 1:
+            color = green
+            broken_streak = False
+        pygame.draw.rect(stripe_in_progress, color, pygame.Rect(pad_amount, midline_y, bar_width, bar_height))
 
+        
+    if broken_streak:
+        if current_streak >= min_streak_length:
+            last_streaks.append(current_streak)
+        if len(last_streaks) > rolling_average_size:
+            last_streaks.pop(0)
+        current_streak = 0    
+    elif button_pressed and not last_button_pressed:
+        current_streak += 1
+        if current_streak >= min_good_streak_length:
+            last_good_streak = current_streak
+        best_streak = max(current_streak, best_streak)
+    
+    last_button_pressed = button_pressed
     button_frame_counter += 1
+            
     
     screen.fill([0,0,0])
     idx = 0
     for sface in history:
         screen.blit(sface, (idx * stripe_width,0))
         idx += 1
-
+    if stripe_in_progress:
+        screen.blit(stripe_in_progress, (idx * stripe_width,0))
+    
     pygame.draw.line(screen, offwhite, [0,midline_y], [window_w,midline_y])
     
     y = stripe_height + 20
